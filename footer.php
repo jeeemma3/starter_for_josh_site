abb7822fe70f701ab8bd5d82ce8fe73aed9abb2a
jeeemma3 <jeeemma3@gmail.com>
2025-03-16 20:35:33 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
-<footer class="container-fluid text-center"><!-- div for the main image to stretch s to the edge of the page-->
+<footer class="container-fluid text-center">
 
-<div class="container"><!-- div to hold the content in the middle of the page-->
-    <div class="row"><!-- a row that gives us access to the BS columns-->
+<div class="container">
+    <div class="row">
         <div class="col-md-6 ">
-           <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span> </a>
+           <a class="navbar-brand" href="http://170.187.231.66/~mesh5/wp-content/uploads/2024/04/footer.jpg"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span> </a>
 
         </div>
         <div class="col-md-6 ">
             <p>Terms and Conditions</p>
-</div><!-- row-->
-    </div><!-- container -->
-</div><!-- container-fluid-->
+</div>
+    </div>
+</div>
 
 </footer>
 <?php wp_footer(); ?>
