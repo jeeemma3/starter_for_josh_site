2e04945d4cf6ae8eb14860b6ff45958edce23f1b
jeeemma3 <jeeemma3@gmail.com>
2025-03-16 20:22:21 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="<?php bloginfo('charset'); ?>">
+<meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="Description" content="Enter your description here">
-
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet"> 
 
 <title>   <?php
-  if (function_exists('is_tag') && is_tag()) {
-     single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
-  elseif (is_archive()) {
-     wp_title(''); echo ' Archive - '; }
-  elseif (is_search()) {
-     echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
-  elseif (!(is_404()) && (is_single()) || (is_page())) {
-     wp_title(''); echo ' - '; }
-  elseif (is_404()) {
-     echo 'Not Found - '; }
-  if (is_home()) {
-     bloginfo('name'); echo ' - '; bloginfo('description'); }
-  else {
-      bloginfo('name'); }
-  if ($paged>1) {
-     echo ' - page '. $paged; }
-?>
+              if (function_exists('is_tag') && is_tag()) {
+                 single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+              elseif (is_archive()) {
+                 wp_title(''); echo ' Archive - '; }
+              elseif (is_search()) {
+                 echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+              elseif (!(is_404()) && (is_single()) || (is_page())) {
+                 wp_title(''); echo ' - '; }
+              elseif (is_404()) {
+                 echo 'Not Found - '; }
+              if (is_home()) {
+                 bloginfo('name'); echo ' - '; bloginfo('description'); }
+              else {
+                  bloginfo('name'); }
+              if ($paged>1) {
+                 echo ' - page '. $paged; }
+           ?>
 </title>
 <meta name="description" content="<?php
     if (is_single() || is_page()) {
@@ -52,40 +52,45 @@
     }
 ?>">
 <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico">
+
 </head>
-
 <body>
-    <?php get_header(); ?> 
-    <header class="container-fluid bg1">
-  <div class="container"></div>
-
-        <nav class="navbar navbar-expand-md navbar-dark">
+    <header class="container-fluid bg1">   <!-- div for the main image the stretches to the edge of the page- -->
+        <div class="container">
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
         <!-- Brand -->
-        <a class="navbar-brand" href="#"><span class="josh">JOSH </span> <span class="orange">WHITKIN</span> </a>
+        <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
       
-        <!-- Toggler/collapsibe Button -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-label="navbar">
-          <span class="navbar-toggler-icon"></span> 
+        <!-- Toggler/collapsible Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
         </button>
       
         <!-- Navbar links -->
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item text-right">
+            <li class="nav-item">
               <a class="nav-link" href="#">HOME</a>
             </li>
-            <li class="nav-item text-right">
+            <li class="nav-item">
               <a class="nav-link" href="#">PROJECTS</a>
             </li>
-            <li class="nav-item text-right">
+            <li class="nav-item">
               <a class="nav-link" href="#">CV</a>
-            </li><li class="nav-item text-right">
+            </li>
+            <li class="nav-item">
                 <a class="nav-link" href="#">CONTACT</a>
               </li>
           </ul>
         </div>
-     </nav>
-     <div class="tag">
-        <h1> Games Design For the future </h1>         
-  </div>
+      </nav> 
+   
+        <div class="tag"><!-- div that holds the content in the middle of the page-->
+                  <h1 > Games Design For the Future </h1><!--  the main tag line-->
+  </div>            
+</div><!-- container-->
+<!—remove all our css styles and any Javascript code. WordPress automatically calls javascript /jquery elsewhere, calls to javascript are not needed in the header or the footer now- UNLESS you have calls to google or typkit fonts- they can stay-->
+
+<!-- very important that this code is added-->
+<?php wp_head(); ?> 
 </header>
