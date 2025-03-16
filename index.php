769a7963ac89b1f147ac2d914c1a89498e04b4a2
jeeemma3 <jeeemma3@gmail.com>
2025-03-16 20:35:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+<?php get_header();?>
          <section class="container-fluid aboutbg text-center">
             <div class="container">
                 <h2> GAMES &amp; LEVEL DESIGNER </h2>
                 <p class="particle">  and particle wizard</p>
                 <p class="about-text"> My name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments. I always tell a story.</p>
-            </div><!-- container-->
+            </div>
             <div class="blocker"></div>
         </section>
     
@@ -13,9 +13,9 @@
     <section class="container-fluid articlesbg">
             <div class="container">
                 <h3 class="text-center">LATEST ARTICLES</h3>
-                <div class="row"><!--  a row that gives us access to the BS columns-->
+                <div class="row">
                   <div class="col-md-4 text-center">
-                      <img class="img-fluid" alt=“portait” src=" http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port1.jpg">
+                      <img class="img-fluid" alt=“woman” src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port1.jpg">
                         <h4 class="article-title">A PORTRAIT</h4>
                         <p class="date" >12 December 2020</p>
                         <p class="article-p"> STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
@@ -23,7 +23,7 @@
                         <p class="readmore">CONTINUE READING</p>
                     </div>
                     <div class="col-md-4 text-center">
-                      <img class="img-fluid" alt=“portait” src=" http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port2.jpg">
+                      <img class="img-fluid" alt=“robot” src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port2.jpg">
                         <h4 class="article-title">A PORTRAIT</h4>
                         <p class="date" >12 December 2020</p>
                         <p class="article-p"> STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
@@ -32,14 +32,14 @@
                     </div>
     
                     <div class="col-md-4 text-center">
-                      <img class="img-fluid" alt=“portait” src=" http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port3.jpg">
+                      <img class="img-fluid" alt=“map” src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port3.jpg">
                         <h4 class="article-title">A PORTRAIT</h4>
                         <p class="date" >12 December 2020</p>
                         <p class="article-p"> STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
                         </p>
                         <p class="readmore">CONTINUE READING</p>
                     </div>
-                    </div><!-- row-->
-                </div><!-- container-->
-               </section><!-- container-fluid-->
+                    </div>
+                </div>
+               </section>
                <?php get_footer();?> 
